710decf18c6d360bf1c2f54920ea2a000e7d271f
mobidick <mobidick@gmail.com>
2023-03-11 16:35:32 +0900

    restore ci/cd pipeline

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 docker_run="docker run"
 
@@ -29,4 +29,4 @@ fi
 docker_run="$docker_run -d -p $INPUT_HOST_PORT:$INPUT_CONTAINER_PORT mysql:$INPUT_MYSQL_VERSION --port=$INPUT_CONTAINER_PORT"
 docker_run="$docker_run --character-set-server=$INPUT_CHARACTER_SET_SERVER --collation-server=$INPUT_COLLATION_SERVER"
 
-bash -c "$docker_run"
+run -c "$docker_run"
